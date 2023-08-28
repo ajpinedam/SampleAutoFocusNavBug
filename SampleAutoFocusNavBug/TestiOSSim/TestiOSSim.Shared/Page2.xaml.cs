eaca196b816ca40c0092e41750b21247227b77aa
Andres Pineda <pineda.andres@gmail.com>
2023-08-28 17:53:19 -0400

    initial commit

---
+++ SampleAutoFocusNavBug/TestiOSSim/TestiOSSim.Shared/Page2.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System;
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace TestiOSSim
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class Page2 : Page
+	{
+		public Page2()
+		{
+			this.InitializeComponent();
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			Frame.GoBack();
+		}
+	}
+}
